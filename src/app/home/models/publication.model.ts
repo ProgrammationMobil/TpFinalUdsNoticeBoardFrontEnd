4c925fbca5c3b40e2ac9b3234dd2da1cff73f41f
wilfriedjordan <tankwewilfried5@gmail.com>
2025-05-16 17:53:00 +0100

    config

---
+++ src/app/home/models/publication.model.ts
@@ -0,0 +1,9 @@
+export class Publication {
+  public id!: number;
+  public title!: string;
+  public description!: string;
+  public createdAt!: Date;
+  public content!:String;
+  public coverImage!: string;
+
+}
