f38ff5dd56e44503612abaa3353225e3e33b9da3
wilfriedjordan <tankwewilfried5@gmail.com>
2025-05-16 02:18:31 +0100

    config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    colors: {
+      primary: '#1DA1F2',
+      secondary: '#14171A',
+      accent: '#657786',
+      background: '#F5F8FA',
+      text: '#14171A',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      Montserrat: ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
